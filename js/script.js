d46c5d1ca964bd6d8f3879cad6c5851634822154
Matthew Akindayo <matthewakinse@gmail.com>
2022-02-20 06:03:02 +0200

    add resume base script and style

---
+++ js/script.js
@@ -0,0 +1,143 @@
+
+//declare english variable equals 2
+
+var english = true;
+
+function ToggleLang() {
+    
+english = (english !== false) ? false : true ;
+
+var i = 0;
+var elem = document.getElementsByTagName("text");
+
+//about me tab begins here
+
+elem[i++].innerHTML = (english !== false) ?"Bio" : "Oma sivu" ;
+elem[i++].innerHTML = (english !== false) ?"Education" : "Kasvatus" ;
+elem[i++].innerHTML = (english !== false) ?"Work Experience" : "Työ kokemus" ;
+elem[i++].innerHTML = (english !== false) ?"Portfolio" : "Salkku" ;
+elem[i++].innerHTML = (english !== false) ?"Contact" : "Ottaa Yhteyttä" ;
+elem[i++].innerHTML = (english !== false) ?"Positive attitude and resourceful person" : "Positiivi asenne ja neuvokas henkilö" ;
+elem[i++].innerHTML = (english !== false) ?"Date of birth:" : "Syntymäpäivää:" ;
+elem[i++].innerHTML = (english !== false) ?"Gender:" : "sukupuoli:" ;
+elem[i++].innerHTML = (english !== false) ?"Github:" : "Github:" ;
+elem[i++].innerHTML = (english !== false) ?"My Portfolio" : "Oma Salkku" ;
+elem[i++].innerHTML = (english !== false) ?"Hire me for your next project" : "Soita minulle seuravaan projektiin" ;
+elem[i++].innerHTML = (english !== false) ?"Email Address" : "Sahkoposti osoite" ;
+elem[i++].innerHTML = (english !== false) ?"LinkedIn Profile" : "LinkedIn Profiili:" ;
+elem[i++].innerHTML = (english !== false) ?"English" : "Englanti" ;
+elem[i++].innerHTML = (english !== false) ?"Finnish" : "Suomi" ;
+elem[i++].innerHTML = (english !== false) ?"Language switch" : "Vaihda Kieltä" ;
+
+// Education tab begins here
+
+elem[i++].innerHTML = (english !== false) ?"Education" : "Koulutus" ;
+elem[i++].innerHTML = (english !== false) ?"till present" : "tähän asti" ;
+elem[i++].innerHTML = (english !== false) ?"Title of qualification awarded:" : "Tutkinnon nimi:" ;
+elem[i++].innerHTML = (english !== false) ?"Principal subjects/Occupational skills covered" : "pääaineita ammatillisia taitoja" ;
+elem[i++].innerHTML = (english !== false) ?"Lapland University of Applied Sciences, Finland" : "Lapin Ammattikorkeakoulu, Suomi" ;
+elem[i++].innerHTML = (english !== false) ?"Title of qualification awarded:" : "Tutkinnon nimi:" ;
+elem[i++].innerHTML = (english !== false) ?"Bachelor of Business Administration" : "Liiketalouden ammattikorkeakoulututkinnon" ;
+elem[i++].innerHTML = (english !== false) ?"Principal subjects/Occupational skills covered:" : "pääaineita ammatillisia taitoja:" ;
+elem[i++].innerHTML = (english !== false) ?"Business studies, System analysis and Design, Programming Skills and Databases, Information Security." : "Yritystutkimus, järjestelmän analyysi ja suunnittelu, ohjelmointitaidot ja tietokannat, tietoturva." ;
+elem[i++].innerHTML = (english !== false) ?"Study Exchange Abroad (03/2013-06/2013):" : "Opiskella Ulkomailla:" ;
+elem[i++].innerHTML = (english !== false) ?"Travel and Tourism Management." : "Matkustus ja Turismi" ;
+elem[i++].innerHTML = (english !== false) ?"Title of qualification awarded:" : "Tutkinnon nimi:" ;
+elem[i++].innerHTML = (english !== false) ?"Diploma in Statistics" : "Tilaston Diplomi" ;
+elem[i++].innerHTML = (english !== false) ?"Principal subjects/Occupational skills covered:" : "pääaineita ammatillisia taitoja:" ;
+elem[i++].innerHTML = (english !== false) ?"Maths, Statistics and Computer science" : "Matematiikka, Tilastotiede ja Tietotekniikka" ;
+elem[i++].innerHTML = (english !== false) ?"Title of qualification awarded:" : "Tutkinnon nimi:" ;
+elem[i++].innerHTML = (english !== false) ?"Diploma in Desktop Publishing" : "Julkaisujärjestelmä Diplomi" ;
+elem[i++].innerHTML = (english !== false) ?"Principal subjects/Occupational skills covered:" : "pääaineita ammatillisia taitoja:" ;
+elem[i++].innerHTML = (english !== false) ?"Title of qualification awarded:" : "Tutkinnon nimi:" ;
+elem[i++].innerHTML = (english !== false) ?"Principal subjects/Occupational skills covered:" : "pääaineita ammatillisia taitoja:" ;
+elem[i++].innerHTML = (english !== false) ?"High School studies" : "Lukio-opinnot" ;
+
+// Experience tab begins here
+
+elem[i++].innerHTML = (english !== false) ?"Work Experience:" : "Työ Kokemus" ;
+elem[i++].innerHTML = (english !== false) ?"Kieratyskeskus, Hämeenlinna" : "Kieratyskeskus, Hämeenlinna" ;
+elem[i++].innerHTML = (english !== false) ?"till present" : "tähän asti" ;
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held:" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Casual Shop assistant" : "Myymäläapulaine" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Engaged with customers in a sincere and friendly manner<br>Organizing store, fixing price tags and cleaning out fitting rooms<br>Actively pursued personal learning and development opportunities" : "Engaged with customers in a sincere and friendly manner<br>Organizing store, fixing price tags and cleaning out fitting rooms<br>Actively pursued personal learning and development opportunities";
+elem[i++].innerHTML = (english !== false) ?"Freelance web design" : "Freelance-sivuston suunnittelu" ;
+elem[i++].innerHTML = (english !== false) ?"till present" : "tähän asti" ;
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held:" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Freelancer" : "Freelancer" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Web design, Graphics, Typing and programming" : "Web-suunnittelu, grafiikka, kirjoittaminen ja ohjelmointi";
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held: " : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Custodian" : "Siivoja" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Facility Maintenance, Tidying and Cleaning of facilities" : "Myymälä ylläpitosiivous, Siistiminen ja Peruspesu " ;
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held:" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"till present" : "tähän asti"
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held:" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Football Reporter" : "Jalkapallo Reportteri" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Reporting live local finnish football match statistics on phone (mostly summer) " : "Raportointi live-paikallinen jalkapallo-ottelutilasto puhelimella (enimmäkseen kesällä)" ;
+elem[i++].innerHTML = (english !== false) ?"Internship" : "Työharjoittelu" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Office Administrative tasks, Web design (Joomla, wordpress), System maintenance and updates" : "Office Hallinnolliset tehtävät, Web-suunnittelu (Joomla, wordpress), Järjestelmän ylläpito ja päivitykset" ;
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Computer Operator/Team Leader" : "Tietokone-Operaattori/Tiimin Johtaja" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities:" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Administrative tasks, Editing and publishing of books, website design, Database management (Navicat MySQL), Team management, Graphics designer, Typist" : "Hallinnolliset tehtävät, Kirjojen muokkaus ja julkaisu, Tietokannan hallinta (Navicat MySQL), Joukkueenhallinta, Graafinen suunnittelija, Typist" ;
+elem[i++].innerHTML = (english !== false) ?"Occupation or Position held" : "Toimi tai Tehtävä:" ;
+elem[i++].innerHTML = (english !== false) ?"Teacher" : "Opettaja" ;
+elem[i++].innerHTML = (english !== false) ?"Main activities and responsibilities" : "Keskeiset tehtävät ja vastuualueet:" ;
+elem[i++].innerHTML = (english !== false) ?"Primary school Teacher" : "Peruskoulun opettaja" ;
+
+// The skills division here
+
+elem[i++].innerHTML = (english !== false) ?"Skills" : "Taidot" ;
+elem[i++].innerHTML = (english !== false) ?"Website Content Management Systems" : "Verkkosivut CMS " ;
+elem[i++].innerHTML = (english !== false) ?"Programming languages" : "Ohjemointi kielet" ;
+elem[i++].innerHTML = (english !== false) ?"Website Frameworks" : "Verkkosivusto puitteet" ;
+elem[i++].innerHTML = (english !== false) ?"Application Software" : "Sovellusohjelma" ;
+elem[i++].innerHTML = (english !== false) ?"Operating system software" : "Käyttöjärjestelmäohjelmisto" ;
+elem[i++].innerHTML = (english !== false) ?"Meter Reading" : "Mittarilukemat" ;
+elem[i++].innerHTML = (english !== false) ?"Awesome" : "Mahtava" ;
+elem[i++].innerHTML = (english !== false) ?"Average" : "keskiarvo" ;
+elem[i++].innerHTML = (english !== false) ?"Basic" : "Perus" ;
+
+// portfolio tab begins here
+
+elem[i++].innerHTML = (english !== false) ?"Purpose: Forum, user management and online sales of books." : "Päämäärä: Foorumi, Käyttäjien hallinta, kirjojen online-myyntti" ;
+elem[i++].innerHTML = (english !== false) ?"Published: 2013" : "Julkaistu: 2013" ;
+elem[i++].innerHTML = (english !== false) ?"Updated: 2017" : "Päivitetty: 2017" ;
+elem[i++].innerHTML = (english !== false) ?"Full view" : "esittää yksityiskohtaisesti" ;
+elem[i++].innerHTML = (english !== false) ?"Purpose: Blogging" : "Päämäärä: Bloggi" ;
+elem[i++].innerHTML = (english !== false) ?"Published:2013" : "Julkaistu: 2013" ;
+elem[i++].innerHTML = (english !== false) ?"Updated:2017" : "Päivitetty: 2017" ;
+elem[i++].innerHTML = (english !== false) ?"Full view" : "esittää yksityiskohtaisesti" ;
+elem[i++].innerHTML = (english !== false) ?"Laundry booking system" : "Pesula-varausjärjestelmä" ;
+elem[i++].innerHTML = (english !== false) ?"Purpose:Laundry booking in apartment" : "Varaus pesuhuone asuntola" ;
+elem[i++].innerHTML = (english !== false) ?"Platform:mrbs" : "Koroke: mrbs" ;
+elem[i++].innerHTML = (english !== false) ?"Published: under-construction" : "Julkaistu: rakenteilla" ;
+elem[i++].innerHTML = (english !== false) ?"Full view" : "esittää yksityiskohtaisesti" ;
+elem[i++].innerHTML = (english !== false) ?"Purpose: Product showcase and media marketing" : " " ;
+elem[i++].innerHTML = (english !== false) ?"Platform: Facebook" : " " ;
+elem[i++].innerHTML = (english !== false) ?"Published: 2013" : "Julkaistu: 2013" ;
+elem[i++].innerHTML = (english !== false) ?"Full view" : "esittää yksityiskohtaisesti" ;
+
+// Contact tab begins here
+
+
+elem[i++].innerHTML = (english !== false) ?"My Address" : "Osoite " ;
+elem[i++].innerHTML = (english !== false) ?"Mobile Number" : "Puhelin numero " ;
+elem[i++].innerHTML = (english !== false) ?"About Me" : "Minusta" ;
+elem[i++].innerHTML = (english !== false) ?"Logical thinking and self-motivated. I like traveling and have an ability to adapt to different circumstances and cultures. I am open to learn and I can't wait to be a part of your project!" : "Looginen ajattelu ja itsensä motivoiminen Pidän matkustamisesta ja kykyä sopeutua erilaisiin olosuhteisiin ja kulttuureihin. Olen avoin oppimiselle ja en voi odottaa olevan osa projektia! " ;
+
+// Footer
+elem[i++].innerHTML = (english !== false) ?"M.O Akindayo - Resume." : "M.O Akindayo - Ansioluettelo" ;
+}
+
+function Print() {
+    alert("Document print (under development :( yet) thanks")
+}
+function Save() {
+    alert("Save document to local storage - under development")
+}
